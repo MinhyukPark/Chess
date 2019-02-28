c5b48673222a2c1bd2b618b4064e8c728373c496
Minhyuk Park <minpark815@gmail.com>
2019-02-28 10:09:51 +0000

    Initial commit

---
+++ js/puzzle.js
@@ -0,0 +1,44 @@
+function getRandomLine(filename){
+    var rawFile = new XMLHttpRequest();
+    var randomLine;
+    rawFile.open("GET", filename, false);
+    rawFile.onreadystatechange = function() {
+        if(rawFile.readyState === 4) {
+	    if(rawFile.status === 200) {
+	        var allText = rawFile.responseText;
+		var split = allText.split('\n');
+		var randomNum = Math.floor(Math.random() * split.length);
+		randomLine = split[randomNum];
+            }
+        }
+    }
+    rawFile.send(null);
+    return randomLine;
+}
+
+
+function GetPuzzle() {
+    var fenTextBox = document.getElementById("FenTextBox");
+    var invisibleBox = document.getElementById("hidden-current-puzzle");
+    fenTextBox.value = getRandomLine('./data/m8n2.txt');
+    invisibleBox.value = fenTextBox.value;
+    var textarea = document.getElementById("PgnTextBox");
+    textarea.value = '';
+    UIChangeFEN()
+}
+
+function ReloadPuzzle() {
+    var fenTextBox = document.getElementById("FenTextBox");
+    var invisibleBox = document.getElementById("hidden-current-puzzle");
+    fenTextBox.value = invisibleBox.value;
+    var textarea = document.getElementById("PgnTextBox");
+    textarea.value = '';
+    UIChangeFEN()
+}
+
+function SavePuzzle() {
+    var fenTextBox = document.getElementById("FenTextBox");
+    var invisibleBox = document.getElementById("hidden-current-puzzle");
+    invisibleBox.value = fenTextBox.value;
+    UIChangeFEN()
+}
---
+++ js/startchess.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+	        g_timeout = 1000;
+	        UINewGame();
+});
